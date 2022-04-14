47df8d19db878a922fce8f7f6f45bfd015d13731
DONDOGDORJ <minjee9009@gmail.com>
2022-04-14 23:57:12 +0900

    blockos busda beldle

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,32 @@
 const path = require("path");
 const postCSSPlugings = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'), 
-    require('autoprefixer')];
+    require('autoprefixer')
+
+    ];
 module.exports ={
     entry: "/app/assets/scripts/app.js",
     output:{
         filename:"bundled.js",
         path: path.resolve(__dirname,"app")
     },
+    // congif devServer we do not need watch : true
+    // we use browser without refresh
+    devServer :{
+      
+        static: {
+            directory: path.join(__dirname, "app")
+          },
+        hot : true, 
+        // hot gdg maani js bolon css iig browserlu inject hiiged refresh hiihguiger tuhain oorchloltuudg haruulj bh
+        port : 3000,
+        host: "0.0.0.0"
+    },
     mode:'development',
-    watch: true,
+    
     module:{
         rules:[
             {
